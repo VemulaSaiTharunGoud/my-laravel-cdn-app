4b9d44063ab7a9fbba94de06f594e01e1cbe14e5
root <root@ip-172-31-2-149.ap-south-1.compute.internal>
2025-06-20 13:57:46 +0000

    Hardcode fallback ASSET_URL for CloudFront

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'asset_url' => env('ASSET_URL', 'https://d2eaqeywqif5m.cloudfront.net'),
 
     /*
     |--------------------------------------------------------------------------
