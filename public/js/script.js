116ddae93ed7dbb2cd7d3da47c73691f7469eff1
root <root@ip-172-31-2-149.ap-south-1.compute.internal>
2025-06-20 13:22:44 +0000

    Add static assets and views for CDN demo

---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+console.log('CDN JS loaded');
---
+++ public/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel CDN Test</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <h1>Hello, Laravel with CDN!</h1>
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
