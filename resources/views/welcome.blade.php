49556091d88bd86a0c2e7a9b3539fb5a23d3d239
root <root@ip-172-31-2-149.ap-south-1.compute.internal>
2025-06-21 08:47:22 +0000

    Optimize welcome page for SEO and performance

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>My Laravel CDN App</title>
-    <link rel="stylesheet" href="https://d2eaqeywqif5m.cloudfront.net/css/app.css">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Fast-loading Laravel app using S3 and CloudFront for static asset delivery." />
+  <meta name="robots" content="index, follow" />
+
+  <title>Fast Laravel CDN App</title>
+
+  <!-- Preload CSS -->
+  <link rel="preload" href="https://d2eaqeywqif5m.cloudfront.net/css/app.css" as="style" onload="this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://d2eaqeywqif5m.cloudfront.net/css/app.css"></noscript>
+
+  <!-- Preload JS -->
+  <link rel="preload" href="https://d2eaqeywqif5m.cloudfront.net/js/app.js" as="script">
 </head>
 <body>
-    <h1>Hello from Laravel with CDN!</h1>
-    <p>This page is styled from CSS served via CloudFront + S3.</p>
+  <h1>Hello CDN-powered Laravel App</h1>
 
-    <button onclick="showMessage()">Click Me</button>
-
-    <script src="https://d2eaqeywqif5m.cloudfront.net/js/app.js"></script>
+  <!-- Defer JS -->
+  <script src="https://d2eaqeywqif5m.cloudfront.net/js/app.js" defer></script>
 </body>
 </html>
 
